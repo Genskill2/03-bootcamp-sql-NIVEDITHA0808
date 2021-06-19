1962301883c0cb4cf54b2280cc0dd184de8a4bab
K V V S K NIVEDITHA <84661024+NIVEDITHA0808@users.noreply.github.com>
2021-06-20 03:03:44 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -3,6 +3,6 @@ DELETE FROM books_subjects
 WHERE books_id IN (
       SELECT subjects.name 
       FROM books,subjects,books_subjects 
-      WHERE books.id = books_subjects.books_id AND subjects.id = books_subjects.subjects_id AND books.title = 'The Great War for Civilisation);
+      WHERE books.id = books_subjects.books_id AND subjects.id = books_subjects.subjects_id AND books.title = 'The Great War for Civilisation');
 
 
