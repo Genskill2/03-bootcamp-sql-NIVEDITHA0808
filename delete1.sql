32d64f47eb0c0cc69aff2d2a1663cf2f7580c3c6
K V V S K NIVEDITHA <84661024+NIVEDITHA0808@users.noreply.github.com>
2021-06-20 03:02:12 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,9 +1,8 @@
  --delete 'history' subject
 DELETE FROM books_subjects 
 WHERE books_id IN (
-      SELECT books_subjects.books_id
-      FROM books_subjects
-      WHERE subjects_id =10);
+      SELECT subjects.name 
+      FROM books,subjects,books_subjects 
+      WHERE books.id = books_subjects.books_id AND subjects.id = books_subjects.subjects_id AND books.title = 'The Great War for Civilisation);
 
-DELETE FROM subjects
-WHERE name ='History';
+
